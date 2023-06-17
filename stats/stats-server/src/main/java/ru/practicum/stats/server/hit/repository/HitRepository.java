ccd21dfc4ec6f20010c2b17efda1ea233b610eb4
nix-pix <nix-pix@yandex.ru>
2023-06-17 14:10:13 +0300

    На ревью Диплом (Этап 1) v2.0

--- stats/stats-server/src/main/java/ru/practicum/stats/server/hit/repository/HitRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/server/hit/repository/HitRepository.java
@@ -1,14 +1,16 @@
-package ru.practicum.stats.server.hit.storage;
+package ru.practicum.stats.server.hit.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.practicum.stats.server.hit.model.Hit;
 import ru.practicum.stats.server.hit.model.Stats;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface HitStorage extends JpaRepository<Hit, Integer> {
+@Repository
+public interface HitRepository extends JpaRepository<Hit, Integer> {
 
     @Query("SELECT new ru.practicum.stats.server.hit.model.Stats(hit.app, hit.uri, COUNT(DISTINCT hit.ip)) " +
             "FROM Hit hit " +
--- stats/stats-server/src/main/java/ru/practicum/stats/server/hit/service/HitServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/server/hit/service/HitServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import ru.practicum.stats.dto.HitDto;
 import ru.practicum.stats.dto.StatsDto;
 import ru.practicum.stats.server.hit.mapper.StatsMapper;
-import ru.practicum.stats.server.hit.storage.HitStorage;
+import ru.practicum.stats.server.hit.repository.HitRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,37 +18,37 @@ import static ru.practicum.stats.server.hit.mapper.HitMapper.toEndpointHitDto;
 @Slf4j
 @Service
 public class HitServiceImpl implements HitService {
-    private final HitStorage hitStorage;
+    private final HitRepository hitRepository;
 
     @Autowired
-    public HitServiceImpl(HitStorage hitStorage) {
-        this.hitStorage = hitStorage;
+    public HitServiceImpl(HitRepository hitRepository) {
+        this.hitRepository = hitRepository;
     }
 
     @Override
     public HitDto createEndpointHit(HitDto endpointHitDto) {
-        return toEndpointHitDto(hitStorage.save(toEndpointHit(endpointHitDto)));
+        return toEndpointHitDto(hitRepository.save(toEndpointHit(endpointHitDto)));
     }
 
     @Override
     public List<StatsDto> getEndpointHits(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
         if (uris == null || uris.isEmpty()) {
             if (unique) {
-                return hitStorage.getAllStatsDistinctIp(start, end).stream()
+                return hitRepository.getAllStatsDistinctIp(start, end).stream()
                         .map(StatsMapper::toViewStatsDto)
                         .collect(toList());
             } else {
-                return hitStorage.getAllStats(start, end).stream()
+                return hitRepository.getAllStats(start, end).stream()
                         .map(StatsMapper::toViewStatsDto)
                         .collect(toList());
             }
         } else {
             if (unique) {
-                return hitStorage.getStatsByUrisDistinctIp(start, end, uris).stream()
+                return hitRepository.getStatsByUrisDistinctIp(start, end, uris).stream()
                         .map(StatsMapper::toViewStatsDto)
                         .collect(toList());
             } else {
-                return hitStorage.getStatsByUris(start, end, uris).stream()
+                return hitRepository.getStatsByUris(start, end, uris).stream()
                         .map(StatsMapper::toViewStatsDto)
                         .collect(toList());
             }
